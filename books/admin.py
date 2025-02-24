e411039f231fc59211175341dab2b571f74eed54
jdomdev <jdomingodev@duck.com>
2025-02-24 20:14:26 +0100

    chore: register Book model in books/admin.py

--- books/admin.py
+++ books/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Book
 
-# Register your models here.
+@admin.register(Book)
+class AdminBook(admin.ModelAdmin):
+    list_display = ('title', 'author', 'date_publication', 'isbn')
+    search_fields = ('title', 'author', 'isbn')
+    list_filter = ('date_publication',)
